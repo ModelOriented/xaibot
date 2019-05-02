e2aaf9e0cc4ac5933caa24e7443f8ef2824e79e4
Przemysław Biecek <przemyslaw.biecek@gmail.com>
2019-05-02 10:04:39 +0200

    fixes in plots

--- plumber/titanic.R
+++ plumber/titanic.R
@@ -95,7 +95,7 @@ function(req, class = "X", gender = "X", age = "X", sibsp = "X", parch = "X", fa
 #* @param embarked passenger embarked. X if missing. One of ("Belfast", "Cherbourg", "Queenstown", "Southampton")
 #* @get /break_down
 #* @post /break_down
-#* @png (width = 400, height = 250)
+#* @png (width = 420, height = 250)
 function(req, class = "X", gender = "X", age = "X", sibsp = "X", parch = "X", fare = "X",
   embarked = "X") {
 
@@ -110,9 +110,14 @@ function(req, class = "X", gender = "X", age = "X", sibsp = "X", parch = "X", fa
   if (length(order) < 1)  order <- "age"
   sp_rf <- break_down(explain_titanic_rf, new_passanger,
                       order = order)
-  sp_rf[nrow(sp_rf),c("contribution", "cummulative")] = sp_rf[nrow(sp_rf)-1,"cummulative"]
-  print(plot(sp_rf) +
-          ggtitle("What influences the survival?", subtitle))
+  sp_rf[nrow(sp_rf) - 1,"contribution"] = sp_rf[nrow(sp_rf) - 1,"contribution"] + sp_rf[nrow(sp_rf),"cummulative"] - sp_rf[nrow(sp_rf) - 1,"cummulative"]
+  sp_rf[nrow(sp_rf) - 1,"cummulative"] = sp_rf[nrow(sp_rf),"cummulative"]
+  pr <- predict(explain_titanic_rf, new_passanger)
+  title = paste0("Your chances of survival are ", round(pr, 3))
+  subtitle = "And here is how I get it"
+  print(plot(sp_rf) + ingredients::theme_drwhy() + facet_null() +
+          theme(legend.position = "none") + 
+          ggtitle(title, subtitle))
 }
 
 
@@ -128,7 +133,7 @@ function(req, class = "X", gender = "X", age = "X", sibsp = "X", parch = "X", fa
 #* @param embarked passenger embarked. X if missing. One of ("Belfast", "Cherbourg", "Queenstown", "Southampton")
 #* @get /ceteris_paribus
 #* @post /ceteris_paribus
-#* @png (width = 400, height = 250)
+#* @png (width = 420, height = 250)
 function(req, variable = "age", class = "X", gender = "X", age = "X", sibsp = "X", parch = "X", fare = "X",
   embarked = "X") {
 
@@ -143,23 +148,33 @@ function(req, variable = "age", class = "X", gender = "X", age = "X", sibsp = "X
   }
 
   if (variable %in% c("age", "sibsp", "parch", "fare")) {
+    pr <- predict(explain_titanic_rf, new_passanger)
+    title = paste0("Your chances of survival for ", variable, " = ", new_passanger[[variable]], " are ", round(pr, 3))
+    subtitle = paste0("But keeping everything else constant \nchances for different values of ", variable, " are...")
     grids = list()
     grids[[variable]] = sort(unique(explain_titanic_rf$data[,variable]))
     cp_titanic_rf <- ceteris_paribus(explain_titanic_rf, new_passanger,
                                      variables = variable, variable_splits = grids)
     pl <- plot(cp_titanic_rf) +
             show_observations(cp_titanic_rf, variables = variable, size = 5) +
-            ylab("survival probability") +
-            ggtitle("Keeping everything else constant...", subtitle)
+          ylab(paste0("Survival after change in ", variable)) + facet_null() +
+          xlab(variable) +
+      ggtitle(title, subtitle) + 
+      theme(plot.title = element_text(size = 12), plot.subtitle = element_text(size = 12))
   }
   if (variable %in% c("class", "gender", "embarked")) {
+    pr <- predict(explain_titanic_rf, new_passanger)
+    title = paste0("Your chances of survival for ", variable, " = ", new_passanger[[variable]], " are ", round(pr, 3))
+    subtitle = paste0("But keeping everything else constant \nchances for different values of ", variable, " are...")
     grids = list()
     grids[[variable]] = sort(unique(explain_titanic_rf$data[,variable]))
     cp_titanic_rf <- ceteris_paribus(explain_titanic_rf, new_passanger,
                                      variables = variable, variable_splits = grids)
     pl <- plot(cp_titanic_rf, only_numerical = FALSE) +
-            ylab("survival probability") +
-            ggtitle("Keeping everything else constant...", subtitle)
+            ylab(paste0("Survival after change in ", variable)) + facet_null() +
+            xlab(variable) +
+            ggtitle(title, subtitle) + 
+            theme(plot.title = element_text(size = 12), plot.subtitle = element_text(size = 12))
   }
 
   print(pl)
